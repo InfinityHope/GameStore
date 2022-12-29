edec0e63bd244f630dde2603047bffa36a9beea4
Nikita <bigbasspoll@gmail.com>
2022-12-29 16:13:28 +0300

    preprodversion

--- src/context/SearchOverlayContext/SearchOverlayContext.ts
+++ src/context/SearchOverlayContext/SearchOverlayContext.ts
@@ -2,7 +2,7 @@ import { ChangeEvent, createContext } from 'react'
 
 export const SearchOverlayContext = createContext({
     searchTerm: '',
-    changeSearchTerm: (e: ChangeEvent<HTMLInputElement> | '') => {},
+    changeSearchTerm: (e: ChangeEvent<HTMLInputElement>) => {},
     activeOverlay: false,
     showOverlay: (value: boolean) => {},
 })
